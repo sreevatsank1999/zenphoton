08a4f65b7c433d18fcb46326e4fd66d1b48fc5a8
Micah Elizabeth Scott <beth@scanlime.org>
2013-04-23 17:52:14 -0700

    Fix common 404 errors

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,20 @@ set -e
 ./build.sh
 
 FILES="\
-    html/zenphoton.js       \
-    html/rayworker.js       \
-    html/rayworker-asm.js   \
-    html/index.html         \
-    html/missing.html       \
-    html/favicon.gif        \
-    html/favicon.ico        \
-    html/logo-57.png        \
-    html/logo-72.png        \
-    html/logo-114.png       \
-    html/logo-144.png       \
-    html/roboto.ttf         "
+    html/zenphoton.js                       \
+    html/rayworker.js                       \
+    html/rayworker-asm.js                   \
+    html/index.html                         \
+    html/missing.html                       \
+    html/favicon.gif                        \
+    html/favicon.ico                        \
+    html/logo-57.png                        \
+    html/logo-72.png                        \
+    html/logo-114.png                       \
+    html/logo-144.png                       \
+    html/apple-touch-icon-precomposed.png   \
+    html/humans.txt                         \
+    html/robots.txt                         \
+    html/roboto.ttf                         "
 
 scp $FILES scanlime@scanlime.org:~/zenphoton.com/
