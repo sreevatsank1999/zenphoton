cf8f5b37c80c91411a7065a07bdb8f90ea9dc520
Sreevatsank Kadaveru <sreevatsank1999@gmail.com>
2021-11-18 01:00:55 -0800

    Added origin and color to `class Path`

--- hqz/src/path.h
+++ hqz/src/path.h
@@ -5,30 +5,41 @@
 class Path
 {
 public:
-    Path();
-    Path(uint32_t path_length_expected);
-    Path(Path &&path);
+    Path(Vec2 o, Color c);
+    Path(Vec2 o, Color c, uint32_t path_length_expected);
+    Path(Path &&p);
 
     Path& operator=(Path &&p);
 
     void append(Vec2 point);
 
 private:
-
+    Vec2 origin;
     std::vector<Vec2> v;    // Vertices in path traced
+    Color color;            // Color of the path
 };
 
-Path::Path(){}
+Path::Path(Vec2 o, Color c)
+    : origin(o),
+    color(c)
+{}
 
-Path::Path(uint32_t path_length_expected){
+Path::Path(Vec2 o, Color c, uint32_t path_length_expected)
+    : Path(o,c)
+{
     v.reserve(path_length_expected);
 }
 
-Path::Path(Path &&path) 
-: v(std::move(path.v)) 
-{}
+Path::Path(Path &&p) 
+: v(std::move(p.v)) 
+{
+    origin = p.origin;
+    color = p.color;
+}
 
 Path& Path::operator=(Path &&p){
+    origin = p.origin;
+    color = p.color;
     v = std::move(p.v);
     return *this;
 };
